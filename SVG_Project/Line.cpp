35eaf004ebc6ed03a11a739e43f73b6c37b3557f
khoikhanh123 <khoiltv4@gmail.com>
2025-08-05 23:40:14 +0700

    Update Line.cpp
    updating definition for methods

--- SVG_Project/Line.cpp
+++ SVG_Project/Line.cpp
@@ -8,38 +8,32 @@ line::line() {
 }
 
 void line::setValue(tinyxml2::XMLElement* element) {
-    const char* attrs[] = { "stroke", "stroke-width", "stroke-opacity" };
+    this->SVGElement::setValue(element);
+    const char* attrs[] = {"x1", "y1", "x2", "y2" };
     for (auto attr : attrs) {
         const char* val = element->Attribute(attr);
         if (val) {
-            if (attr == "stroke-opacity") this->strokeOpacity = atof(val);
-            else if (attr == "stroke-width") this->strokeWidth = atoi(val);
-            else if (attr == "stroke") {
-                stringstream s(val);
-                string token = "";
-                getline(s, token, '(');
-                getline(s, token, ',');
-                BYTE red = stoi(token);
-                getline(s, token, ',');
-                BYTE green = stoi(token);
-                getline(s, token, ')');
-                BYTE blue = stoi(token);
-                this->stroke = Color(red, green, blue);
-            }
+            if (attr == "x1") this->vertices[0].X = atoi(element->Attribute("x1"));
+            else if (attr == "y1") this->vertices[0].Y = atoi(element->Attribute("y1"));
+            else if (attr == "x2") this->vertices[1].X = atoi(element->Attribute("x2"));
+            else if (attr == "y2") this->vertices[1].Y = atoi(element->Attribute("y2"));
         }
     }
-    this->vertices[0].X = atoi(element->Attribute("x1"));
-    this->vertices[0].Y = atoi(element->Attribute("y1"));
-    this->vertices[1].X = atoi(element->Attribute("x2"));
-    this->vertices[1].X = atoi(element->Attribute("y2"));
 }
 
 void line::draw(HDC hdc) {
     Graphics g(hdc);
-
     //Draw Line
+    this->draw(&g);
+}
+void line::draw(Graphics* g) {
+    this->handleTransform(g);
+    //Pen
     Pen pen(Color(this->strokeOpacity * 255, this->stroke.GetR(), this->stroke.GetG(), this->stroke.GetB()));
+    //Brush
+    SolidBrush brush(Color(this->fillOpacity * 255, this->fill.GetR(), this->fill.GetG(), this->fill.GetB()));
     pen.SetWidth(this->strokeWidth);
-    g.DrawLine(&pen, this->vertices.front(), this->vertices.back());
+    pen.SetBrush(&brush);
+    g->DrawLine(&pen, this->vertices.front(), this->vertices.back());
 }
 
