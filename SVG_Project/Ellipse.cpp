6d7781060bbbb6d82443385f7a585b508aff328b
khoikhanh123 <khoiltv4@gmail.com>
2025-08-05 23:37:08 +0700

    Update Ellipse.cpp
    Changing method's definition

--- SVG_Project/Ellipse.cpp
+++ SVG_Project/Ellipse.cpp
@@ -2,12 +2,12 @@
 
 
 //Ellipse
-ellipse::ellipse() {
+ellipse::ellipse() : SVGElement() {
 	this->height = this->width = 0;
 }
 
 void ellipse::setValue(tinyxml2::XMLElement* element) {
-	this->shape::setValue(element);
+	this->SVGElement::setValue(element);
 	const char* attrs[] = { "cx", "cy", "rx", "ry" };
 	for (auto attr : attrs) {
 		const char* val = element->Attribute(attr);
@@ -21,23 +21,26 @@ void ellipse::setValue(tinyxml2::XMLElement* element) {
 }
 
 void ellipse::draw(HDC hdc) {
-	Graphics graphic(hdc);
-	//Trans
-	GraphicsState state = graphic.Save();
-	this->handleTransform(&graphic);
+	Graphics g(hdc);
+	this->draw(&g);
+}
 
+void ellipse::draw(Graphics* g) {
+	this->handleTransform(g);
+	//Fill
 	SolidBrush brush(Color(this->fillOpacity * 255, this->fill.GetR(), this->fill.GetG(), this->fill.GetB()));
-	graphic.FillEllipse(&brush, this->origin.X, this->origin.Y, this->width, this->height);
+	g->FillEllipse(&brush, this->origin.X, this->origin.Y, this->width, this->height);
 	//Draw Outline
 	Pen pen(Color(this->strokeOpacity * 255, this->stroke.GetR(), this->stroke.GetG(), this->stroke.GetB()));
 	pen.SetWidth(this->strokeWidth);
-	graphic.DrawEllipse(&pen, this->origin.X, this->origin.Y, this->width, this->height);
+	g->DrawEllipse(&pen, this->origin.X, this->origin.Y, this->width, this->height);
 }
 
 //Circle
+circle::circle() : ellipse() {}
 
 void circle::setValue(tinyxml2::XMLElement* element) {
-	this->shape::setValue(element);
+	this->SVGElement::setValue(element);
 	const char* attrs[] = { "cx", "cy", "r" };
 	for (auto attr : attrs) {
 		const char* val = element->Attribute(attr);
