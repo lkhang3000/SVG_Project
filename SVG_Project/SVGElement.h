0293c865582a89684544f44158c3accf55cbb510
khoikhanh123 <khoiltv4@gmail.com>
2025-08-05 23:33:15 +0700

    Update SVGElement.h
    adding new attributes for SVGElement due to the delete of Shape class, now Shape derived classes will inherit directly from SVGElement

--- SVG_Project/SVGElement.h
+++ SVG_Project/SVGElement.h
@@ -4,6 +4,7 @@
 
 
 #include <iostream>
+#include <string>
 #include <objidl.h>
 #include <windows.h>
 #include <gdiplus.h>
@@ -11,15 +12,27 @@
 #include <vector>
 #include "tinyxml2.h"
 
+using namespace std;
+using namespace Gdiplus;
+
+#pragma comment (lib,"Gdiplus.lib")
 
 class SVGElement {
+protected:
+	string transform;
+	double fillOpacity;
+	double strokeOpacity;
+	Color fill;
+	Color stroke;
+	int strokeWidth;
 public:
-	virtual void setValue(tinyxml2::XMLElement* element) = 0;
+	SVGElement();
+	void handleTransform(Graphics* graphics);
+	void deleteTransform();
+	virtual void setValue(tinyxml2::XMLElement* element);
 	virtual void draw(HDC hdc) = 0;
+	virtual void draw(Graphics* g) = 0;
+	
 };
 
-
-
-
 #endif // !_SVGELEMENT_
-
