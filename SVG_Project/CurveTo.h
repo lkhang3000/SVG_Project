fe128f347fc91b12902c20d6db2b984c28115105
Lê Sĩ Toàn <24127558@student.hcmus.edu.vn>
2025-08-20 21:20:46 +0700

    Update CurveTo.h for S/s command

--- SVG_Project/CurveTo.h
+++ SVG_Project/CurveTo.h
@@ -3,11 +3,17 @@
 #include "PathCommand.h"
 
 class CurveTo : public PathCommand {
-    Point p1, p2, p3;
 public:
-    CurveTo(Point P1, Point P2, Point P3): p1(P1), p2(P2), p3(P3) {}
-    CurveTo(int x1, int y1, int x2, int y2, int x3, int y3) : p1(Point(x1, y1)), p2(Point(x2, y2)), p3(Point(x3, y3)) {
-	}
+    Point p1, p2, p3;
+    CurveTo(Point P1, Point P2, Point P3) : p1(P1), p2(P2), p3(P3) {}
+    CurveTo(float x1, float y1, float x2, float y2, float x3, float y3)
+        : p1(Point(x1, y1)), p2(Point(x2, y2)), p3(Point(x3, y3)) {
+    }
+
+    // Accessors for reflection in S/s command
+    float x2() const { return p2.X; }
+    float y2() const { return p2.Y; }
+
     void draw(Gdiplus::Graphics* graphics, Gdiplus::GraphicsPath* path) override;
 };
 #endif
